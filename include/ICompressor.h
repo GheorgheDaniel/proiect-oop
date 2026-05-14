0854f8340df20fd3decc017f506f3e65fd137419
GheorgheDaniel <123456789+odin@users.noreply.github.com>
2026-05-14 22:38:39 +0300

    add ICompressor interface for polymorphism

---
+++ include/ICompressor.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+class ICompressor{
+    public:
+    virtual ~ICompressor() = default;
+
+    virtual void compress(const std::string& inputFilePath, const std::string& outputFilePath) = 0;
+    virtual void decompress(const std::string& inputFilePath, const std::string&outputFilePath) = 0;
+};
+
+
