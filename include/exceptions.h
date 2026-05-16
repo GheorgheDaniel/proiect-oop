15f1b25273dfe0259b20baec9529ef3c1c58bd98
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-16 22:45:38 +0000

    exceptions added, manages the errors

---
+++ include/exceptions.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <exception>
+#include <string>
+
+class ArchiveException : public std::exception {
+    protected:
+    std::string message;
+    public:
+    ArchiveException(const std::string& msj) {
+        message = msj;
+    }
+
+    const char* what() const noexcept override{
+        return message.c_str();
+    }
+};
+
+class FileMissingE : public ArchiveException{
+    public: 
+    FileMissingE(const std::string& file) : ArchiveException("Fisier lipsa: " + file) {}
+};
+
+class CorruptArchiveE : public ArchiveException{
+    public: 
+    CorruptArchiveE() : ArchiveException("Fisierul binar este corupt") {}
+};
