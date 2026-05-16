135b2f69c018cb50ff85c8431ce6e97852f87399
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-16 22:44:39 +0000

    archivemanager added, manages the options for compress/decompress

---
+++ src/archivemanager.cpp
@@ -0,0 +1,99 @@
+#include "archivemanager.h"
+#include "huffman.h"
+#include "lzw.h"
+#include "compressordecider.h"
+#include "info.h"
+#include <filesystem>
+#include <bits/stdc++.h>
+
+size_t ArchiveManager::byteseconomisiti = 0;
+
+void ArchiveManager::compress(const std::string& input) {
+    Info<std::string>::actions("COMPRESIA A INCEPUT", input);
+    std::ifstream fin(input, std::ios::binary);
+    std::string input1 = input;
+    /*
+        de creat fisierul de iesire
+        de testat care compresie e mai buna
+        de aplicat compresia respectiva
+    */
+    std::transform(input1.begin(), input1.end(), input1.begin(), [](unsigned char c) {
+        return std::tolower(c);
+    });
+    std::string v; 
+    if(input1.find(".") < input1.size())
+    while(input1.back() != '.') {v.push_back(input1.back()); input1.pop_back();}
+    reverse(v.begin(), v.end());
+    if(mode == 1 || mode == 2) {
+        AH.set_data(v, mode);
+        std::ofstream fout(input1 + "bin", std::ios::binary);
+        fout << AH;
+        fout.close();
+        // Huffman HF;
+        // HF.compress(input, input1 + "bin");
+        Info<int>::actions("SE RULEAZA ALGORITMUL: ", AH.get_algo_type());
+        ICompressor* compresor = CompressorDecider::createCompressor(AH.get_algo_type());
+        compresor->compress(input, input1 + "bin");
+        delete compresor;
+    }
+    if(mode == 3) {
+        AH.set_data(v, 1);
+        std::ofstream foutH("huffman" + input1 + "bin", std::ios::binary);
+        foutH << AH;
+        foutH.close();
+        Info<int>::actions("SE RULEAZA ALGORITMUL: ", AH.get_algo_type());
+        ICompressor* compH = CompressorDecider::createCompressor(AH.get_algo_type());
+        compH->compress(input, "huffman" + input1 + "bin");
+        delete compH;
+
+        AH.set_data(v, 2);
+        std::ofstream foutL("LZW" + input1 + "bin", std::ios::binary);
+        foutL << AH;
+        foutL.close();
+        Info<int>::actions("SE RULEAZA ALGORITMUL: ", AH.get_algo_type());
+        ICompressor* compL = CompressorDecider::createCompressor(AH.get_algo_type());
+        compL->compress(input, "LZW" + input1 + "bin");
+        delete compL;
+
+        size_t sizeh = std::filesystem::file_size("huffman" + input1 + "bin");
+        size_t sizelzw = std::filesystem::file_size("LZW" + input1 + "bin");
+
+        if(sizeh <= sizelzw) {
+            std::filesystem::remove("LZW" + input1 + "bin");
+            std::filesystem::rename("huffman" + input1 + "bin", input1 + "bin");
+        }
+        else {
+            std::filesystem::remove("huffman" + input1 + "bin");
+            std::filesystem::rename("LZW" + input1 + "bin", input1 + "bin");
+        }
+    }
+    byteseconomisiti += std::filesystem::file_size(input) - std::filesystem::file_size(input1 + "bin");
+}
+
+void ArchiveManager::decompress(const std::string& input) {
+    Info<std::string>::actions("DECOMPRESIA A INCEPUT", input);
+    std::ifstream fin(input, std::ios::binary);
+    std::string input1 = input;
+    if(input1.find(".") < input1.size())
+    while(input1.back() != '.') {input1.pop_back();}
+
+    fin >> AH;
+    size_t alreadyread = fin.tellg();
+    fin.close();
+
+    Info<int>::actions("SE RULEAZA ALGORITMUL: ", AH.get_algo_type());
+    ICompressor* compresor = CompressorDecider::createCompressor(AH.get_algo_type());
+    compresor->decompress(input, input1 + AH.get_extension(), alreadyread);
+    Huffman* huff1 = dynamic_cast<Huffman*>(compresor);
+
+    if(huff1 != nullptr) {
+        huff1->clearTree();
+    }
+
+    delete compresor;
+    /*
+        de verificat ce compresie e 
+        de decompresat
+    */
+
+}
