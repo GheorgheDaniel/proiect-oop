a7a2e4ed53629bfaa37d7f24fdefd018b33c9bd4
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-16 22:51:44 +0000

    archive manager added, class that manages the options from the user:

---
+++ include/archivemanager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "archiveheader.h"
+
+class ArchiveManager{
+    private: 
+    int mode;
+    archive_header AH;
+    static size_t byteseconomisiti;
+    public:
+    ArchiveManager(int m = 1)  {
+        mode = m;
+    }
+    static size_t getbytese() { return byteseconomisiti; }
+    void compress(const std::string& in);
+    void decompress(const std::string& in);
+};
