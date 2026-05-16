371f834a65dc6d305dc7176c64d1d155c8859f35
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-16 22:48:10 +0000

    node structure added

---
+++ include/node.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct Node{
+    Node *left, *right;
+    int val;
+    int fr;
+    Node() {
+        fr = 0;
+        left = nullptr; right = nullptr;
+    }
+    bool operator <(const Node& v) const{
+        return this->fr > v.fr;
+    }
+
+    Node operator +(Node& v) {
+        Node ans;
+        ans.fr = v.fr + this->fr;
+        ans.left = this;  
+        ans.right = &v;
+        return ans;
+    }
+};  
