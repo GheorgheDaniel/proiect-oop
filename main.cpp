4a319f915239fe835b9b79ba3005ea86d91356e8
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-17 17:39:07 +0000

    fix: main project name changed

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ int main() {
         std::cout << " 5. Afiseaza statistici si IESI\n";
         std::cout << " Alege o optiune (1-5): ";
         std::cin >> optiune;
+        if(std::cin.eof()) {
+            break;
+        }
         if (optiune == 5) {
             std::cout << ArchiveManager::getbytese() << '\n';
             std::cout << "Iesire din program. O zi buna!\n";
